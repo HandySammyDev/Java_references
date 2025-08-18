2ad2a850696ca036ce99a1236973b123bfa62e7e
Samuel Perez-Hernandez <lsamuelwell@gmail.com>
2025-08-18 15:01:12 -0400

    Add files via upload

---
+++ My2DAdventure/out/production/My2DAdventure/entity/Entity.java
@@ -0,0 +1,16 @@
+package entity;
+
+import java.awt.image.BufferedImage;
+
+public class Entity {
+
+    public int worldX, worldY;
+    public int speed;
+
+    //It describes an Image with an accessible buffer of image data. (we use this to store our image files)
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public String direction;
+
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
+}
