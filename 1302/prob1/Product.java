c66151139efb3e07b52d6e0e10c6c94f8d85157d
Samuel Perez-Hernandez <lsamuelwell@gmail.com>
2025-09-01 09:52:17 -0400

    Add files via upload

--- 1302/prob1/Product.java
+++ 1302/prob1/Product.java
@@ -13,43 +13,51 @@ public class Product {
     }
 
     private int extractBatch(){
-        return Integer.parseInt(extractDate());
+
+        return Integer.parseInt(code.substring(indexDateBegins+8));
     }
     private String extractDate(){
-        indexDateBegins = date.indexOf(date.length()-4);
 
-        return date.substring(indexDateBegins);
+        int i = indexDateBegins;
+
+        return code.substring(i,i+2) + "/" + code.substring(i+2,i+4) + "/" + code.substring(i+4,i+8);
     }
     private String extractPlant(){
-        return plant;
+        if(Character.isLetter(code.charAt(2))){
+            indexDateBegins = 2;
+            return code.substring(0,2);
+        }
+        else{
+            indexDateBegins = 1;
+            return code.substring(0,1);
+        }
     }
 
     public int getBatch(){
-        return batch;
+        return batch = extractBatch();
     }
     public String getCode(){
         return code;
     }
     public String getDate(){
-        return date;
+        return date = extractDate();
     }
     public String getPlant(){
-        return plant;
+        return plant = extractPlant();
     }
 
     public boolean isAfter2000(){
 
-        return extractBatch() >= 2000;
+        return batch >= 2000;
     }
     public boolean isMonthEqual(int month){
-        //Returns true if month the product was produced is equal to the input month.
-        return false;
+        return Integer.parseInt(getDate().substring(0,2)) == month;
     }
 
     @Override
     public String toString(){
 //        code=VLD040419367, plant=VLD, date=04/04/1936, batch=7
-//        code=SF0404193643, plant=SF, date=04/04/1936, batch=43   <---????
+//        code=SF0404193643, plant=SF, date=04/04/1936, batch=43
 
         return String.format("code=%s, plant=%s, date=%s, batch%d",
                             getCode(), getPlant(), getDate(), getBatch());
