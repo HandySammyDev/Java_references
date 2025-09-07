1a69472462958b9334b70be44fb9100c8001167d
Samuel Perez <lsamuelwell@gmail.com>
2025-09-07 15:02:04 -0400

    hw_02 added

---
+++ 1302/hw_02/Store.java
@@ -0,0 +1,44 @@
+package prob1;
+
+//
+
+public class Store {
+
+    private Employee[] emps;
+    private int numEmps;
+
+    Store(){
+        numEmps = 0;
+    }
+
+    public void addEmployee(Employee e){
+
+    }
+    public Employee getEmployee(int i){
+
+    }
+    public Employee getEmployeeWithName(String findName){
+
+    }
+    public int getNumEmployees(){
+
+    }
+    public double getTotalHours(){
+
+    }
+    public double getTotalPay(){
+
+    }
+    public Employee removeEmployee(){
+
+    }
+
+    @Override
+    public String toString(){
+        return "";
+    }
+
+    public static void main(String[] args){
+
+    }
+}
